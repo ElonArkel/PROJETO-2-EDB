f3c3c59a4a4105cc4af625d83fb8cb446a9885fb
Vinicinn <viniciusmelo554@gmail.com>
2025-01-10 01:19:28 -0300

    changes accept

--- avl.h
+++ avl.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef AVL_H
 #define AVL_H
 typedef struct NO *ArvAVL;
@@ -13,21 +12,3 @@ void RotacaoLR(ArvAVL *raiz); // rotação dupla a esquerda
 void RotacaoRL(ArvAVL *raiz); // rotação dupla a direita
 
 #endif
-=======
-#ifndef AVL_H
-#define AVL_H
-typedef struct NO *ArvAVL;
-
-int altura_NO(struct NO *no);
-int fatorBalanceamento_NO(struct NO *no);
-int maior(int x, int y);
-int estaVazia(ArvAVL *raiz);
-void RotacaoLL(ArvAVL *raiz); // rotação a esquerda
-void RotacaoRR(ArvAVL *raiz); // rotação a direita
-void RotacaoLR(ArvAVL *raiz); // rotação dupla a esquerda
-void RotacaoRL(ArvAVL *raiz); // rotação dupla a direita
-int Insere_ArvAVL(ArvAVL *raiz, int valor);
-int Remove_ArvAVL(ArvAVL *raiz, int valor);
-
-#endif
->>>>>>> origin/main
